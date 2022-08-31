66c157df5527f86fddfe5cb609b579d5b894b6a9
robertmwenja4 <robertmwenja4@gmail.com>
2022-08-31 23:24:15 +0300

    Login Creating

---
+++ app/src/main/java/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bankapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/bankapp/authenticate/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.bankapp.authenticate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bankapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
