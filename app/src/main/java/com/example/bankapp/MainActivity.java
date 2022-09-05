b5348c88b26337744f136538101d3a11b89b31a8
robertmwenja4 <robertmwenja4@gmail.com>
2022-09-05 18:56:56 +0300

    Mobile Number Added

--- app/src/main/java/com/example/bankapp/MainActivity.java
+++ app/src/main/java/com/example/bankapp/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.bankapp.viewModel.MpesaViewModel;
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button mbtn;
     private MpesaViewModel viewModel;
+    String m_Text;
+    EditText mphone;
+    private static String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mbtn = findViewById(R.id.activate);
         mbtn.setOnClickListener(this);
+        mphone = findViewById(R.id.phoneNumber);
 
     }
 
@@ -35,22 +43,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void Activate() {
+        String no = mphone.getText().toString();
+        if (TextUtils.isEmpty(no)){
+            mphone.setError("Email is required");
+            mphone.requestFocus();
+            return;
+        }
+        String number = "254"+no.substring(1,10);
+        Toast.makeText(this, "The number is: "+number, Toast.LENGTH_SHORT).show();
         viewModel = ViewModelProviders.of(this).get(MpesaViewModel.class);
-        viewModel.makeApiCall();
+        viewModel.makeApiCall(number);
         showDialog();
     }
 
     public void showDialog(){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Response");
-        alert.setMessage("Your Request has been Received, You will get a confirmation within 24hrs!!");
-        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        alert.setMessage("Your Request has been Received Pay & You will get a confirmation within 24hrs!!");
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         });
-        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
